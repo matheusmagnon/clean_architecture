d425825fa5df1bbd6c87d440a1e3331840705e13
Matheus Magno <matheusmagnon@gmail.com>
2025-04-11 09:49:49 -0300

    chore: ident code

--- src/infra/db/entities/users.py
+++ src/infra/db/entities/users.py
@@ -5,16 +5,12 @@ from src.infra.db.settings.base import Base
 
 
 class Users(Base):
-
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
 
-
-age = Column(Integer, nullable=False)
-
-
-def __repr__(self):
-    return f"User [id={self.id}, first_name={self.first_name}]"
+    def __repr__(self):
+        return f"User [id={self.id}, first_name={self.first_name}]"
