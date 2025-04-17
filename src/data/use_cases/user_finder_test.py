4b9ed65a8ecad2cf4fcd8f46fe945d0962385412
Matheus Magno <matheusmagnon@gmail.com>
2025-04-17 10:11:09 -0300

    feat: add use cases

---
+++ src/data/use_cases/user_finder_test.py
@@ -0,0 +1,63 @@
+from src.infra.db.tests.users_repository import UsersRepositorySpy
+from .user_finder import UserFinder
+
+
+def test_find():
+    first_name = 'myName'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+
+    print()
+    assert repo.select_user_attributes["first_name"] == first_name
+
+    assert response["type"] == "Users"
+    assert response["count"] == len(response["attributes"])
+    assert response["attributes"] != ['']
+
+
+def test_find_error_in_valid_name():
+    '''Validation for erro'''
+    first_name = 'myNam3'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Nome inválido para busca"
+
+
+def test_find_error_in_long_name():
+    '''Validation for long name error '''
+    first_name = 'MyNameISVerylooooong'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Nome muito grande para busca"
+
+
+def test_find_error_user_not_found():
+    class UsersRepositoryError(UsersRepositorySpy):
+        def select_user(self, first_name: str):
+            return []
+
+    first_name = 'myNotFoundName'
+
+    repo = UsersRepositoryError()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Usuário não encontrado"
