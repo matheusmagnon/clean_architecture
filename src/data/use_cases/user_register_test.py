af4535dcf8d50f3df5dee35a030c3ff4d445b6a4
Matheus Magno <matheusmagnon@gmail.com>
2025-05-06 10:04:27 -0300

    feat: implement user register test

---
+++ src/data/use_cases/user_register_test.py
@@ -0,0 +1,36 @@
+from src.infra.db.tests.users_repository import UsersRepositorySpy
+from .user_register import UserRegister
+
+
+def test_register():
+    first_name = 'John'
+    last_name = 'Doe'
+    age = 30
+
+    repo = UsersRepositorySpy()
+    user_register = UserRegister(repo)
+
+    response = user_register.register(first_name, last_name, age)
+
+    assert repo.insert_user_attributes["first_name"] == first_name
+    assert repo.insert_user_attributes["last_name"] == last_name
+    assert repo.insert_user_attributes["age"] == age
+
+    assert response["type"] == "Users"
+    assert response["count"] == 1
+    assert response["attributes"]
+
+
+def test_register_first_name_error():
+    first_name = 'John123'
+    last_name = 'Doe'
+    age = 30
+
+    repo = UsersRepositorySpy()
+    user_register = UserRegister(repo)
+
+    try:
+        user_register.register(first_name, last_name, age)
+        assert False
+    except Exception as exception:
+        assert str(exception) == "Nome inválido para busca"
